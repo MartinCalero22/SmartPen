421d82bc94dd17b98ec8c5dcc8171f6a6b29123d
MartinCalero22 <marbru2207@gmail.com>
2026-09-05 13:46:47 -0300

    Agregar archivos de proyecto.

---
+++ SmartPen/Boligrafo.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartPen
+{
+    internal class Boligrafo
+    {
+        // Atributos
+        public int capacidadTintaMaxima;
+        public int grosorPunta;
+        public string color;
+        public int cantidadTinta;
+
+        //Constructor
+
+        public Boligrafo(string color, int grosorPunta)
+        {
+            this.capacidadTintaMaxima = 100;
+            this.cantidadTinta = 80;
+            this.color = color;
+            this.grosorPunta = grosorPunta;
+        }
+
+        //Metodo
+
+        public string Escribir(string texto)
+        {
+            int tintaGastada;
+            if (this.grosorPunta > 10)
+            {
+                tintaGastada = texto.Length * 2;
+
+            }
+            else
+            {
+                tintaGastada = texto.Length;
+            }    
+
+            if (this.cantidadTinta >= tintaGastada)
+            {
+                this.cantidadTinta = this.cantidadTinta - tintaGastada;
+                return texto;
+            }
+            else
+            {
+                return "ERROR!!! No alcanza la tinta.";
+            }
+
+        }
+
+        public string RecargarTinta(int tinta)
+        {
+            while (tinta <= 0 || this.cantidadTinta + tinta > this.capacidadTintaMaxima)
+            {
+                if (tinta <= 0)
+                {
+                    Console.WriteLine("Error!!! Tiene que ingresar un valor positivo a recargar.");
+                }
+                else
+                {
+                    Console.WriteLine("Error!!! La cantidad seleccionada supera la capacidad maxima. (100)");
+                }
+
+                Console.WriteLine("Ingrese nuevamente la cantidad de tinta a recargar: ");
+
+                tinta = int.Parse(Console.ReadLine());
+            }
+
+            this.cantidadTinta = this.cantidadTinta + tinta;
+
+            return "Lapicera Recargada";
+
+        }
+
+        public string INFO()
+        {
+            return $"La capacidad es de: {capacidadTintaMaxima}, la cantidad actual es: {cantidadTinta}, el color de su Bolifrago es {color}, su grosor es {grosorPunta}";
+        }
+
+
+
+    }
+}
---
+++ SmartPen/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace SmartPen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Boligrafo Boli1 = new Boligrafo("Azul", 5);
+            Boligrafo Boli2 = new Boligrafo("Rojo", 15);
+
+            Console.WriteLine("Metodo Mostrar Información");
+            Console.WriteLine(Boli1.INFO());
+            Console.WriteLine(Boli2.INFO());
+            Console.WriteLine("=========================================");
+            Console.WriteLine("Metodo Escribir");
+            Console.WriteLine(Boli1.Escribir("Entrega super tardía"));
+            Console.WriteLine(Boli1.INFO());
+            Console.WriteLine("=========================================");
+            Console.WriteLine(Boli2.Escribir("Entrega aún más tardía"));
+            Console.WriteLine(Boli2.INFO());
+            Console.WriteLine("=========================================");
+            Console.WriteLine("Metodo Recargar");
+            Console.WriteLine(Boli1.RecargarTinta(10));
+            Console.WriteLine(Boli1.INFO());
+            Console.WriteLine("=========================================");
+            Console.WriteLine(Boli2.RecargarTinta(100));
+            Console.WriteLine(Boli2.INFO());
+
+
+
+        }
+    }
+}
